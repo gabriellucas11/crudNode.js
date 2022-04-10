5aad118175271260f6c9d15a8caa33395f03ebaa
Gabriellucas11 <gabriel69.lucas@gmail.com>
2022-04-10 09:45:20 -0300

    crud node

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require ('express');
+
+
+const server = express();
+
+
+server.use(express.json());
+//CRUD --> Criar, ler, atualizar, deletar 
+//insominia testador de rotas
+const cursos=['fulStack Master','desenvolvimento de games', 'viver de youtube'];
+
+//retorna um curso
+server.get('/cursos/:index', (req,res)=>{
+    const {index} = req.params;
+    return res.json(cursos[index]);
+});
+
+
+//retornar todos os cursos
+server.get('/cursos', (req,res)=>{
+    return res.json(cursos)
+});
+
+
+//criar um novo curso
+server.post('/cursos', (req, res)=>{
+    const {name} = req.body;
+    cursos.push(name);
+
+    return res.json(cursos);
+
+});
+
+// atualizar um curso 
+server.put('/cursos/:index',(req,res)=>{
+    const {index}= req.params;
+    const {name}= req.body;
+
+    cursos[index] = name 
+
+    return res.json(cursos);
+});
+
+//deletar um curso 
+
+server.delete('/cursos/:index', (req,res)=>{
+    const {index} = req.params;
+
+    cursos.splice(index,1);
+    return res.json({message: 'O curso foi deletado'});
+});
+
+
+
+
+
+
+
+
+server.listen(3009);
